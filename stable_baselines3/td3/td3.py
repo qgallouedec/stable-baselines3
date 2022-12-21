3609371623d9afd21540933c84ca117642563eb2
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-21 22:35:29 +0100

    fix dim

--- stable_baselines3/td3/td3.py
+++ stable_baselines3/td3/td3.py
@@ -173,9 +173,7 @@ class TD3(OffPolicyAlgorithm):
                 # Compute the next Q-values: min over all critics targets
                 next_q_values = th.stack(self.critic_target(replay_data.next_observations, next_actions), dim=1)
                 next_q_values, _ = th.min(next_q_values, dim=1)
-                target_q_values = (
-                    replay_data.rewards.squeeze(1) + (1 - replay_data.dones.squeeze(1)) * self.gamma * next_q_values
-                )
+                target_q_values = replay_data.rewards + (1 - replay_data.dones) * self.gamma * next_q_values
 
             # Get current Q-values estimates for each critic network
             current_q_values = self.critic(replay_data.observations, replay_data.actions)
