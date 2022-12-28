66c1dba2a312a75a83d7bf1c35064572de7b8172
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-28 11:48:54 +0100

    Monitor inner env reward

--- stable_baselines3/common/vec_env/vec_monitor.py
+++ stable_baselines3/common/vec_env/vec_monitor.py
@@ -50,6 +50,7 @@ class VecMonitor(VecEnvWrapper):
 
         VecEnvWrapper.__init__(self, venv)
         self.episode_returns = None
+        self.inner_episode_returns = None
         self.episode_lengths = None
         self.episode_count = 0
         self.t_start = time.time()
@@ -69,25 +70,34 @@ class VecMonitor(VecEnvWrapper):
     def reset(self) -> VecEnvObs:
         obs = self.venv.reset()
         self.episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.inner_episode_returns = np.zeros(self.num_envs, dtype=np.float32)
         self.episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
         return obs
 
     def step_wait(self) -> VecEnvStepReturn:
         obs, rewards, dones, infos = self.venv.step_wait()
         self.episode_returns += rewards
+        self.inner_episode_returns += infos.get("env_reward", 0)
         self.episode_lengths += 1
         new_infos = list(infos[:])
         for i in range(len(dones)):
             if dones[i]:
                 info = infos[i].copy()
                 episode_return = self.episode_returns[i]
+                inner_episode_return = self.inner_episode_returns[i]
                 episode_length = self.episode_lengths[i]
-                episode_info = {"r": episode_return, "l": episode_length, "t": round(time.time() - self.t_start, 6)}
+                episode_info = {
+                    "r": episode_return,
+                    "i": inner_episode_return,
+                    "l": episode_length,
+                    "t": round(time.time() - self.t_start, 6),
+                }
                 for key in self.info_keywords:
                     episode_info[key] = info[key]
                 info["episode"] = episode_info
                 self.episode_count += 1
                 self.episode_returns[i] = 0
+                self.inner_episode_returns[i] = 0
                 self.episode_lengths[i] = 0
                 if self.results_writer:
                     self.results_writer.write_row(episode_info)
